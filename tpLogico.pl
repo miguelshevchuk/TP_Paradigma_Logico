046f9ed7948d6eb914c450aac228054454b97841
miguelshevchuk <shevchuk.miguel@gmail.com>
2017-06-24 00:41:07 -0300

    Arreglos

--- tpLogico.pl
+++ tpLogico.pl
@@ -4,135 +4,111 @@ persona(maria).
 persona(pedro).
 persona(chameleon).
 
-propiedad('Tinsmith Circle 1774').
-propiedad('Av. Moreno 708').
-propiedad('Av. Siempre Viva 742').
-propiedad('Calle Falsa 123').
+propiedad(Propiedad) :-
+  tiene(Propiedad, _).
+
+% conjuntoInstalaciones(Instalaciones).
 
 tiene('Tinsmith Circle 1774', ambientes(3)).
-tiene('Tinsmith Circle 1774', jardin()).
-tiene('Tinsmith Circle 1774', instalacion('Aire Acondicionado')).
-tiene('Tinsmith Circle 1774', instalacion('Extractor de aire')).
-tiene('Tinsmith Circle 1774', instalacion('Calefacción a gas')).
+tiene('Tinsmith Circle 1774', jardin).
+tiene('Tinsmith Circle 1774', instalaciones(['Aire Acondicionado', 'Extractor de aire', 'Calefacción a gas'])).
 precio('Tinsmith Circle 1774', 700).
 
 tiene('Av. Moreno 708', ambientes(7)).
-tiene('Av. Moreno 708', jardin()).
+tiene('Av. Moreno 708', jardin).
 tiene('Av. Moreno 708', piscina(30)).
-tiene('Av. Moreno 708', instalacion('Aire Acondicionado')).
-tiene('Av. Moreno 708', instalacion('Extractor de aire')).
-tiene('Av. Moreno 708', instalacion('Calefacción por loza radiante')).
-tiene('Av. Moreno 708', instalacion('Vidrios dobles')).
+tiene('Av. Moreno 708', instalaciones(['Aire Acondicionado', 'Extractor de aire', 'Calefacción por loza radiante', 'Vidrios dobles'])).
 precio('Av. Moreno 708', 2000).
 
 tiene('Av. Siempre Viva 742', ambientes(4)).
-tiene('Av. Siempre Viva 742', jardin()).
-tiene('Av. Siempre Viva 742', instalacion('Calefacción a gas')).
+tiene('Av. Siempre Viva 742', jardin).
+tiene('Av. Siempre Viva 742', instalaciones(['Calefacción a gas'])).
 precio('Av. Siempre Viva 742', 1000).
 
 tiene('Calle Falsa 123', ambientes(3)).
 precio('Calle Falsa 123', 2000).
 
 quiere(carlos, ambientes(3)).
-quiere(carlos, jardin()).
+quiere(carlos, jardin).
 quiere(ana, piscina(100)).
-quiere(ana, instalacion('Aire Acondicionado')).
-quiere(ana, instalacion('Vidrios dobles')).
+quiere(ana, instalaciones(['Aire Acondicionado', 'Vidrios dobles'])).
 quiere(maria, piscina(15)).
 quiere(maria, ambientes(2)).
 
-quiere(pedro, instalacion('Vidrios dobles')).
-quiere(pedro, instalacion('Calefacción por loza radiante')).
+quiere(pedro, instalaciones(['Vidrios dobles', 'Calefacción por loza radiante'])).
 quiere(pedro, Deseo) :-
   quiere(maria, Deseo).
 
 quiere(chameleon, Deseo) :-
-  quiere(Alguien, Deseo),
-  Alguien \= chameleon.
+  Alguien \= chameleon,
+  quiere(Alguien, Deseo).
 
 inicializarPropPersona(Propiedad, Persona) :-
   persona(Persona),
   propiedad(Propiedad).
 
 cumple(Propiedad, ambientes(AmbientesDeseados)) :-
+  quiere(_, ambientes(AmbientesDeseados)),
   tiene(Propiedad, ambientes(Cantidad)),
   Cantidad >= AmbientesDeseados.
 
 cumple(Propiedad, piscina(MetrosPiletaDeseados)) :-
   tiene(Propiedad, piscina(MetrosPileta)),
+  quiere(_, piscina(MetrosPiletaDeseados)),
   MetrosPileta >= MetrosPiletaDeseados.
+
+cumple(Propiedad,instalaciones(InstalacionesDeseadas)):-
+  % conjuntoInstalaciones(Instalaciones),
+    quiere(_, instalaciones(InstalacionesDeseadas)),
+		tiene(Propiedad, instalaciones(Instalaciones)),
+		forall(member(Instalacion, InstalacionesDeseadas), member(Instalacion,Instalaciones)).
 
 cumple(Propiedad, Caracteristica) :-
   tiene(Propiedad, Caracteristica).
 
-propiedadCumpleCaracteristicas(Propiedad, Persona) :-
-  quiere(Persona, Deseo),
-  cumple(Propiedad, Deseo).
+caracteristicaQueNoCumpleNingunaPropiedad(Caracteristica) :-
+  quiere(_,Caracteristica),
+  not(cumple(_, Caracteristica)).
 
 cumpleTodo(Propiedad, Persona) :-
-  inicializarPropPersona(Propiedad, Persona),
+  persona(Persona),
+  propiedad(Propiedad),
   forall(quiere(Persona,Deseo), cumple(Propiedad, Deseo)).
 
 mejorOpcion(Propiedad, Persona) :-
-  inicializarPropPersona(Propiedad, Persona),
-  cumpleTodo(Propiedad, Persona),
-  findall(Prop, (cumpleTodo(Prop, Persona), elPrecio1EsMenorQueEl2(Prop, Propiedad) ), Propiedades),
-  length(Propiedades,Cantidad),
-  Cantidad is 0.
-
-elPrecio1EsMenorQueEl2(Propiedad2, Propiedad) :-
-  precio(Propiedad, Precio),
-  Propiedad2 \= Propiedad,
-  precio(Propiedad2, Precio2),
-  Precio2 < Precio.
-
-% todosCumplenTodo(Persona, Propiedades) :-
-%   findall(Prop, (cumpleTodo(Prop, Persona), Prop \= Propiedad), Propiedades),}.
+  cumpleTodo(Propiedad,Persona),
+  precio(Propiedad,Precio),
+  forall(cumpleTodo(Propiedad2, Persona), (precio(Propiedad2, Precio2),  Precio =< Precio2)).
 
 mejorOpcion2(Propiedad, Persona) :-
-  inicializarPropPersona(Propiedad, Persona),
   cumpleTodo(Propiedad, Persona),
-  findall(Prop, (cumpleTodo(Prop, Persona), Prop \= Propiedad), Propiedades),
-  precioMenorEnTodaLaLista(Propiedad, Propiedades).
-
-precioMenorEnTodaLaLista(Propiedad, []).
-precioMenorEnTodaLaLista(Propiedad, [Propiedad2 | Cola]) :-
-  precioMenorEnTodaLaLista(Precio, Cola),
-  elPrecio1EsMenorQueEl2(Propiedad2, Propiedad).
-
-% mejorOpcion2(Propiedad, Persona) :-
-%   inicializarPropPersona(Propiedad, Persona),
-%   cumpleTodo(Propiedad, Persona),
-%   precio(Propiedad, Precio),
-%   forall(precio(Propiedad2,Precio2), (cumpleTodo(Propiedad2, Persona), Propiedad2 \= Propiedad, Precio < Precio2)).
+  precio(Propiedad, Precio),
+  not((cumpleTodo(Propiedad2, Persona), precio(Propiedad2, Precio2), Precio2 < Precio)).
 
 estaSatisfecho(Persona) :-
-  persona(Persona),
-  findall(Prop, cumpleTodo(Prop, Persona), Propiedades),
-  length(Propiedades,Cantidad),
-  Cantidad > 0.
+  cumpleTodo(_, Persona).
 
-filtrarPersonas(Condicion, Personas) :-
-  findall(Persona, call(Condicion, Persona), Personas).
+conjuntoSegun(Condicion, EntidadesSinRepetir) :-
+  findall(Entidad, call(Condicion, Entidad), Entidades),
+  list_to_set(Entidades, EntidadesSinRepetir).
 
 efectividad(Efectividad):-
-  filtrarPersonas(estaSatisfecho, PersonasSatisfechas),
-  filtrarPersonas(persona, Personas),
+  conjuntoSegun(estaSatisfecho, PersonasSatisfechas),
+  conjuntoSegun(persona, Personas),
   length(PersonasSatisfechas, CantSatisfechos),
   length(Personas, CantPersonas),
   Efectividad is CantSatisfechos / CantPersonas.
 
-% esPropiedadChicaa(Propiedad):-
-%   not(tiene(Propiedad,ambientes(A))).
+esPropiedadChicaa(Propiedad):-
+  propiedad(Propiedad),
+  not(tiene(Propiedad,ambientes(_))).
 
 esPropiedadChica(Propiedad):-
-  propiedad(Propiedad),
-  tiene(Propiedad,ambientes(Ambientes)),
-  Ambientes =< 1.
+  tiene(Propiedad,ambientes(1)).
 
 esPropiedadTop(Propiedad):-
-  not(esPropiedadChica(Propiedad)),
-  tiene(Propiedad, instalacion('Aire Acondicionado')).
+  cumple(Propiedad, instalaciones(['Aire Acondicionado'])),
+  not(esPropiedadChica(Propiedad)).
 
 
 %%%%%%%%%%%%%%%%    CONSULTAS    %%%%%%%%%%%%%%%%%
@@ -156,19 +132,31 @@ esPropiedadTop(Propiedad):-
 % Deseo = piscina(15).
 
 %4)
-% ?- cumple(Propiedad, ambientes(2)).
-% Propiedad = 'Tinsmith Circle 1774' ;
-% Propiedad = 'Av. Moreno 708' ;
-% Propiedad = 'Av. Siempre Viva 742' ;
-% Propiedad = 'Calle Falsa 123' ;
-% false.
+% ?- cumple(_, ambientes(2)).
+% true .
 
 %5)
-% ?- propiedadCumpleCaracteristicas(Propiedad,pedro).
-% Propiedad = 'Tinsmith Circle 1774' ;
-% Propiedad = 'Av. Moreno 708' ;
-% Propiedad = 'Av. Siempre Viva 742' ;
-% Propiedad = 'Calle Falsa 123' ;
+% ?- cumple(Propiedad, Deseo), quiere(pedro, Deseo).
+% Propiedad = 'Tinsmith Circle 1774',
+% Deseo = ambientes(2) ;
+% Propiedad = 'Av. Moreno 708',
+% Deseo = ambientes(2) ;
+% Propiedad = 'Av. Siempre Viva 742',
+% Deseo = ambientes(2) ;
+% Propiedad = 'Calle Falsa 123',
+% Deseo = ambientes(2) ;
+% Propiedad = 'Tinsmith Circle 1774',
+% Deseo = ambientes(2) ;
+% Propiedad = 'Av. Moreno 708',
+% Deseo = ambientes(2) ;
+% Propiedad = 'Av. Siempre Viva 742',
+% Deseo = ambientes(2) ;
+% Propiedad = 'Calle Falsa 123',
+% Deseo = ambientes(2) ;
+% Propiedad = 'Av. Moreno 708',
+% Deseo = piscina(15) ;
+% Propiedad = 'Av. Moreno 708',
+% Deseo = piscina(15) ;
 % false.
 
 %6)
@@ -176,33 +164,29 @@ esPropiedadTop(Propiedad):-
 % Persona = carlos,
 % Deseo = ambientes(3) ;
 % Persona = carlos,
-% Deseo = jardin() ;
+% Deseo = jardin ;
 % Persona = maria,
 % Deseo = piscina(15) ;
+% Persona = maria,
+% Deseo = piscina(15) ;
+% Persona = maria,
+% Deseo = ambientes(2) ;
 % Persona = maria,
 % Deseo = ambientes(2) ;
 % Persona = pedro,
 % Deseo = piscina(15) ;
 % Persona = pedro,
-% Deseo = ambientes(2) ;
-% Persona = chameleon,
-% Deseo = ambientes(3) ;
-% Persona = chameleon,
-% Deseo = jardin() ;
-% Persona = chameleon,
 % Deseo = piscina(15) ;
-% Persona = chameleon,
+% Persona = pedro,
 % Deseo = ambientes(2) ;
-% Persona = chameleon,
-% Deseo = piscina(15) ;
-% Persona = chameleon,
+% Persona = pedro,
 % Deseo = ambientes(2) ;
-% ERROR: Out of local stack
+% false.
 
 % 7)
-% ?- quiere(_,Deseo), not(cumple(_, Deseo)).
-% Deseo = piscina(100) ;
-% Deseo = piscina(100) ;
+% ?- caracteristicaQueNoCumpleNingunaPropiedad(Caracteristica).
+% Caracteristica = piscina(100) ;
+% false.
 
 %8)
 % ?- cumpleTodo(Propiedad,Persona).
@@ -219,25 +203,36 @@ esPropiedadTop(Propiedad):-
 % false.
 
 %9)
-% ?- mejorOpcion(Propiedad, Persona).
+% ?- mejorOpcion2(Propiedad, Persona).
+% Propiedad = 'Tinsmith Circle 1774',
+% Persona = carlos ;
 % Propiedad = 'Tinsmith Circle 1774',
 % Persona = carlos ;
 % Propiedad = 'Av. Moreno 708',
 % Persona = maria ;
 % Propiedad = 'Av. Moreno 708',
+% Persona = maria ;
+% Propiedad = 'Av. Moreno 708',
+% Persona = maria ;
+% Propiedad = 'Av. Moreno 708',
 % Persona = pedro ;
+% Propiedad = 'Av. Moreno 708',
+% Persona = pedro ;
+% Propiedad = 'Av. Moreno 708',
+% Persona = pedro ;
+% Propiedad = 'Tinsmith Circle 1774',
+% Persona = chameleon ;
+% Propiedad = 'Tinsmith Circle 1774',
+% Persona = chameleon ;
 % false.
 
 %10)
 % ?- efectividad(Efectividad).
-% Efectividad = 0.6.
+% Efectividad = 0.8.
 
-% Gracias al polimorfismo, no necesitamos hacer cambios de codigo para la consulta solicitada.
-% Como las instalaciones que se agregan, no precisaban un tratamiento diferenciado en el predicado "cumple",
-% hicimos buen uso del polimorfismo parametrico, y no tuvimos que modificar el predicado ya mencionado.
+% Gracias al polimorfismo ad-hoc, solo tuvimos que agregar una clausula de "cumple" para cumplir con los requerimientos.
+% Tambien hicimos buen uso del polimorfismo parametrico, en el predicado "cumpleTodo".
 
 %11)
 % ?- esPropiedadTop(Propiedad).
-% Propiedad = 'Tinsmith Circle 1774' ;
-% Propiedad = 'Av. Moreno 708' ;
 % false.
